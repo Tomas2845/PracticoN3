46171d314670e3025f3c3493a6ae690c32d5b354
Tomacho <tomacho28.th@gmail.com>
2024-07-24 16:39:11 -0300

    terminamos ejerciso 1

---
+++ js/ejercisio1.js
@@ -0,0 +1,9 @@
+
+const meses = ['enero','febrero','marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+document.write("<h2> Lista de meses </h2>")
+
+document.write("<ul>");
+for ( let i=0; i < meses.length; i++){
+    document.write("<li>"+ meses[i] + "</li>");
+}
+document.write("</ul>");
