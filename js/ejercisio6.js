a4881784e86451746d24afa9b66f416479046e9a
Tomacho <tomacho28.th@gmail.com>
2024-07-24 20:25:31 -0300

    listo ejercisio 6

--- js/ejercisio6.js
+++ js/ejercisio6.js
@@ -1,9 +1,9 @@
-const ladoa = prompt("ingrese el primer valor del lado A del rectangulo");
-const ladob = prompt("ingrese el primer valor del lado B del rectangulo")
+const ladoa = parseInt(prompt("ingrese el primer valor del lado A del rectangulo"));
+const ladob = parseInt(prompt("ingrese el primer valor del lado B del rectangulo"));
 
 function perimetro(ladoa,ladob) 
 {
-    const resultado =  2* (ladoa + ladob)
+    let resultado =  2 *(ladoa + ladob);
     document.write("El perimetro del rectangulo es: " + resultado )
 }
 
